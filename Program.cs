ce9dfb0481a7abcc7da8ae82c41162cfc5b95d4f
R. G. Esteves <rgesteves@gmail.com>
2018-11-27 22:33:12 -0800

    Initial import

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace namedpipetest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	  // adjust whether to behave as a server or as a client based on command line flags
+	  if (args.Length == 1) {
+	     Client(args[0]);
+	  } else {
+  	     Server();
+	  }
+          Console.WriteLine("Done!");
+	  Environment.Exit(0);
+        }
+
+	static void Client(string arg)
+	{
+	   Console.WriteLine("Should be running as a client!");
+	}
+
+	static void Server()
+	{
+	   Console.WriteLine("Should be running as a server!");
+	}
+    }
+}
