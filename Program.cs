6542dc131fb1fb479f832a6305440ef6850777ee
R. G. Esteves <rgesteves@gmail.com>
2018-11-28 00:47:43 -0800

    Fleshed out implementation

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.IO;
+using System.IO.Pipes;
 
 namespace namedpipetest
 {
+
     class Program
     {
-        static void Main(string[] args)
+        const string pipeName = "PipeSample";
+
+	static void Main(string[] args)
         {
 	  // adjust whether to behave as a server or as a client based on command line flags
 	  if (args.Length == 1) {
@@ -17,14 +21,37 @@ namespace namedpipetest
 	  Environment.Exit(0);
         }
 
-	static void Client(string arg)
+	static void Client(string server)
 	{
-	   Console.WriteLine("Should be running as a client!");
+	   Console.WriteLine("Running as client!");
+	   // Ignoring :args server: for now, assuming both processes live in the same box
+	   using (var client = new NamedPipeClientStream(".", pipeName, PipeDirection.In)) {
+	   	 Console.Write($"Connecting to {server}");
+		 client.Connect();
+		 Console.WriteLine("Connected");
+
+		 using (var reader = new StreamReader(client)) {
+		       var message = reader.ReadLine();
+		       Console.WriteLine($"Received message: [{message}]");
+		 }
+	   }
 	}
 
 	static void Server()
 	{
-	   Console.WriteLine("Should be running as a server!");
+	   Console.WriteLine("Running as server!");
+	   using (NamedPipeServerStream ps = new NamedPipeServerStream(pipeName,
+								       PipeDirection.Out)) {
+	    Console.Write("Waiting for connection...");
+	    ps.WaitForConnection();
+	    Console.WriteLine("Connected!");
+
+	    using (var writer = new StreamWriter(ps)) {
+	    	  Console.Write("Enter message: ");
+		  var message = Console.ReadLine();
+		  writer.WriteLine(message);
+	    }
+}
 	}
     }
 }
