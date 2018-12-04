1998163011526f2a1a6508e43d6e9c5290d4cda1
R. G. Esteves <rgesteves@gmail.com>
2018-12-03 16:25:38 -0800

    Rough communication with subprocess

--- tsclient/src/index.ts
+++ tsclient/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+import * as cp from 'child_process';
+
+let proc : cp.ChildProcess = cp.spawn('wc', ['-c'], {'stdio' : ['pipe']});
+
+proc.stdout.on('data', (data) => { console.log(`Received some data (${data}) from subprocess`); })
+proc.stdout.on('error', (data) => { console.log("Got an error"); })
+
+proc.stdin.write('hello world');
+proc.stdin.end();		
+
+console.log("Done!");
